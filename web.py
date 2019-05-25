977b5b866a17aa74eabc3f4d2c6e42dc4ee98641
Toxic Cat <14369594+Toxic-Cat@users.noreply.github.com>
2019-05-26 01:04:36 +0800

    Update web.py

--- web.py
+++ web.py
@@ -2,14 +2,19 @@ import os
 import sys
 import uuid
 import logging
-from logging import Formatter, FileHandler
-from flask import Flask, request, jsonify
 from ocr_core import process_image
+from werkzeug import secure_filename
+from flask import Flask, request, jsonify
+from logging import Formatter, FileHandler
 
 app = Flask(__name__)
 app.config['CACHE_FOLDER'] = '~/ocr_image_cache/'
 app.config['ALLOWED_EXTENSIONS'] = set(['tiff', 'bmp', 'png', 'jpg', 'jpeg', 'webp'])
 app.config['VERSION'] = 1.0
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 @app.route('/v{}/ocr'.format(app.config['VERSION']), methods=['GET', 'POST'])
 def ocr():
@@ -17,9 +22,8 @@ def ocr():
         if 'image' not in request.files:
             return jsonify({"status":"error","message":"Empty request!"})
         uploaded_image = request.files['image']
-        extension = os.path.splitext(uploaded_image.filename)[1]
-        if extension in app.config['ALLOWED_EXTENSIONS']:
-            cache_file_name = str(uuid.uuid4()) + extension
+        if uploaded_image and allowed_file(uploaded_image.filename):
+            cache_file_name = str(uuid.uuid4()) + '_' + uploaded_image.filename
             uploaded_image.save(os.path.join(app.config['CACHE_FOLDER'], cache_file_name))
             ocr_output = process_image(uploaded_image)
             return jsonify({"status":"success","ocr_content":ocr_output})
