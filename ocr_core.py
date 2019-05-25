d9dd180639d29cd94bf100c19fb3b5770b5c15f1
Toxic Cat <14369594+Toxic-Cat@users.noreply.github.com>
2019-05-26 01:23:09 +0800

    Update ocr_core.py

--- ocr_core.py
+++ ocr_core.py
@@ -1,9 +1,8 @@
 import pytesseract
 from PIL import Image
 from PIL import ImageFilter
-from io import StringIO
 
 def process_image(image):
-    image_cache = Image.open(StringIO(image))
+    image_cache = Image.open(image)
     image_cache.filter(ImageFilter.SHARPEN)
     return pytesseract.image_to_string(image_cache)
