d01cc92643216d0c67955f03387064c59da169ab
Toxic Cat <14369594+Toxic-Cat@users.noreply.github.com>
2019-05-26 01:49:48 +0800

    Update web.py

--- web.py
+++ web.py
@@ -8,7 +8,7 @@ from logging import Formatter, FileHandler
 
 app = Flask(__name__)
 app.config['CACHE_FOLDER'] = '~/ocr_image_cache/'
-app.config['ALLOWED_EXTENSIONS'] = set(['tiff', 'bmp', 'png', 'jpg', 'jpeg', 'webp', 'TIFF', 'BMP', 'PNG', 'JPG', 'JPEG', 'WEBP'])
+app.config['ALLOWED_EXTENSIONS'] = set(['tiff', 'bmp', 'png', 'jpg', 'jpeg', 'webp'])
 app.config['VERSION'] = 1.0
 
 def allowed_file(filename):
@@ -21,7 +21,7 @@ def ocr():
         if 'image' not in request.files:
             return jsonify({"status":"error","message":"Empty request!"})
         uploaded_image = request.files['image']
-        if uploaded_image and allowed_file(uploaded_image.filename):
+        if uploaded_image and allowed_file(str.lower(uploaded_image.filename)):
             ocr_output = process_image(uploaded_image)
             return jsonify({"status":"success","ocr_content":ocr_output})
         else:
